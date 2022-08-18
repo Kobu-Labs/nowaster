6f1b1100b3ef9837d57dbee1a01f8aab3bf3eda2
Kobu <thekobu@gmail.com>
2022-08-18 20:21:26 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/entries/", response_model=List[schemas.TrackEntry])
+def read_users(db: Session = Depends(get_db)):
+    users = crud.get_track_entries(db)
+    return users
+
+@app.get("/entries/{category}", response_model=List[schemas.TrackEntry])
+def read_user(category: str, db: Session = Depends(get_db)):
+    entries = crud.get_track_entries_by_category(db, category=category)
+    if entries is None:
+        raise HTTPException(status_code=404, detail="No entries with given category")
+    return entries
+
+@app.post("/entries/", response_model=schemas.TrackEntry)
+def create_entry(track_entry: schemas.TrackEntryCreate, db: Session = Depends(get_db)):
+    return crud.create_track_entry(db=db, track_entry_create=track_entry) 
+
---
+++ src/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+
+from .database import Base
+
+class TrackEntry(Base):
+    __tablename__ = "tracks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    category = Column(String, nullable=False)
+    duration = Column(Integer, nullable=False)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=False)
+    description = Column(String)
+
