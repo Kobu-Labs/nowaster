a797cec8b12cd54fd9164fcc5e9a5ee82503ddee
Jakub Jakubec <thekobu@gmail.com>
2022-09-17 21:24:51 +0200

    change startup command

--- main.py
+++ main.py
@@ -8,7 +8,3 @@ app = FastAPI()
 app.include_router(api_router)
 app.include_router(root_router)
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
