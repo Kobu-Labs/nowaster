4ef5a7e70aefeea389a37817a49e3ec950abcc90
Jakub Jakubec <thekobu@gmail.com>
2022-09-24 20:39:45 +0200

    add database url as env variable (#18)

--- app/db.py
+++ app/db.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./master_database.db"
+from app.config import settings
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    settings.database_url,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
