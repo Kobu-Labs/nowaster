ac71353e6de674f4688b5079cee9b086761e9ac0
Jakub Jakubec <thekobu@gmail.com>
2023-02-09 00:09:52 +0100

    add a post process hook for the db env variable (#23)

--- app/db.py
+++ app/db.py
@@ -1,12 +1,5 @@
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from app.config import settings
-
-engine = create_engine(
-    settings.database_url,
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+SessionLocal = sessionmaker()
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+from collections.abc import Callable
+from typing import Optional
+
 from fastapi import APIRouter, FastAPI
+from sqlalchemy import create_engine
+from starlette.types import Receive, Scope, Send
 
 from app.api.api import api_router
-from app.db import Base, engine
+from app.db import Base, SessionLocal
+from config import settings
 
-Base.metadata.create_all(bind=engine)
 
-root_router = APIRouter()
-app = FastAPI()
+class Nowaster:
+    __root_router = APIRouter()
+    __app = FastAPI()
 
-app.include_router(api_router)
-app.include_router(root_router)
+    def __init__(
+        self, postprocess_db_url: Optional[Callable[[str], str]] = None
+    ) -> None:
+        self.__app.include_router(api_router)
+        self.__app.include_router(self.__root_router)
+
+        if postprocess_db_url is None:
+            db_url = settings.database_url
+        else:
+            db_url = postprocess_db_url(settings.database_url)
+        engine = create_engine(db_url)
+        Base.metadata.create_all(bind=engine)
+        SessionLocal.configure(bind=engine)
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        await self.__app(scope, receive, send)
